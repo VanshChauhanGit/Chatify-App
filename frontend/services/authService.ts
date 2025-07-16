e922518fbb923476d5278c76cf847d0c775e332e
VanshChauhanGit <vanshchauhan1106@gmail.com>
2025-07-16 23:27:31 +0530

    Add authentication context and service for login and registration

---
+++ frontend/services/authService.ts
@@ -0,0 +1,37 @@
+import { API_URL } from "@/constants";
+import axios from "axios";
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_URL}/auth/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.log("got error:", error);
+    const msg = error?.response?.data?.message || "Login Failed!";
+    throw new Error(msg);
+  }
+};
+
+export const register = async (
+  name: string,
+  email: string,
+  password: string,
+  avatar?: string | null
+) => {
+  try {
+    const response = await axios.post(`${API_URL}/auth/register`, {
+      name,
+      email,
+      password,
+      avatar,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.log("got error:", error);
+    const msg = error?.response?.data?.message || "Registration Failed!";
+    throw new Error(msg);
+  }
+};
