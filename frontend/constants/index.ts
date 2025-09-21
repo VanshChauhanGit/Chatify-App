a3aee5878d01f3af9d5177fafbe8bb475b0ee2a6
VanshChauhanGit <vanshchauhan1106@gmail.com>
2025-09-21 22:02:18 +0530

    fix: Update API_URL to use production endpoint and comment out local configurations

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,9 +1,11 @@
-import { Platform } from "react-native";
+// import { Platform } from "react-native";
 
-export const API_URL =
-  Platform.OS == "android"
-    ? "http://192.168.31.243:3000"
-    : "http://localhost:3000";
+// export const API_URL =
+//   Platform.OS == "android"
+//     ? "http://192.168.31.243:3000"
+//     : "http://localhost:3000";
+
+export const API_URL = "https://chatify-b.vercel.app/";
 
 export const EXPO_CLIENT_ID =
   "834490667497-3k6q1e7jabgcbccjpet0vnghbg5mosk6.apps.googleusercontent.com";
