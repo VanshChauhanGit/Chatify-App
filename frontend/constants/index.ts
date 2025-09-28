859a9fa378344486147cfe112741bcaabd47e9d3
VanshChauhanGit <vanshchauhan1106@gmail.com>
2025-09-28 13:14:43 +0530

    fix: streamline API and Cloudinary constants with environment variable fallbacks

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,29 +1,17 @@
-// import { Platform } from "react-native";
+export const API_URL = process.env.API_URL || "https://chatify-b.vercel.app/";
 
-// export const API_URL =
-//   Platform.OS == "android"
-//     ? "http://192.168.31.243:3000"
-//     : "http://localhost:3000";
-
-// export const API_URL = "https://chatify-b.vercel.app/";
-
-// export const EXPO_CLIENT_ID =
-//   "834490667497-3k6q1e7jabgcbccjpet0vnghbg5mosk6.apps.googleusercontent.com";
-// export const ANDROID_CLIENT_ID =
-//   "834490667497-486es90r7c2kesk14tm08v2e4a22dreh.apps.googleusercontent.com";
-// export const WEB_CLIENT_ID =
-//   "834490667497-vm8rohsl67kuu91rha67hr3s058q7ls2.apps.googleusercontent.com";
-// export const IOS_CLIENT_ID = "";
-
-// export const CLOUDINARY_CLOUD_NAME = "dyx5wayln";
-// export const CLOUDINARY_UPLOAD_PRESET = "images";
-
-export const API_URL = process.env.API_URL;
-
-export const EXPO_CLIENT_ID = process.env.EXPO_CLIENT_ID;
-export const ANDROID_CLIENT_ID = process.envANDROID_CLIENT_ID;
-export const WEB_CLIENT_ID = process.env.WEB_CLIENT_ID;
+export const EXPO_CLIENT_ID =
+  process.env.EXPO_CLIENT_ID ||
+  "834490667497-3k6q1e7jabgcbccjpet0vnghbg5mosk6.apps.googleusercontent.com";
+export const ANDROID_CLIENT_ID =
+  process.env.ANDROID_CLIENT_ID ||
+  "834490667497-486es90r7c2kesk14tm08v2e4a22dreh.apps.googleusercontent.com";
+export const WEB_CLIENT_ID =
+  process.env.WEB_CLIENT_ID ||
+  "834490667497-vm8rohsl67kuu91rha67hr3s058q7ls2.apps.googleusercontent.com";
 export const IOS_CLIENT_ID = "";
 
-export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-export const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET;
+export const CLOUDINARY_CLOUD_NAME =
+  process.env.CLOUDINARY_CLOUD_NAME || "dyx5wayln";
+export const CLOUDINARY_UPLOAD_PRESET =
+  process.env.CLOUDINARY_UPLOAD_PRESET || "images";
